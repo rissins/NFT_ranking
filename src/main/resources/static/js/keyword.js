adcc28fc1f4be5a8583030696e5db42d0428fe97
ckdgus08 <ckdgus0808@naver.com>
2021-12-04 00:29:09 +0900

    style: 로딩 이미지 추가 / 키워드간 간격 추가 / 마우스 올렸을때 크기 조정

--- src/main/resources/static/js/keyword.js
+++ src/main/resources/static/js/keyword.js
@@ -1,39 +1,35 @@
 function getKeyword() {
-    $.ajax({
-        method: 'GET',
-        url: '/crawling',
-        dataType: 'json',
-        success: function (data) {
-            if (data.length > 0) {
-                let target = document.getElementById("target")
-                data = data.slice(0, 40).sort(() => Math.random() - 0.5);
+    let target = document.getElementById("target")
 
-                for (let i = 0; i < 40; i++) {
-                    let word = data[i].word
-                    let count = data[i].count
+    loadingImage(target);
+    axios.get('/crawling')
+        .then((response) => {
+            let words = response.data.slice(0, 40).sort(() => Math.random() - 0.5);
 
-                    target.innerHTML += "<span class='word' style='font-size: " + (count * 3) + "px'> " + word + " </span>"
-                }
+            for (let i = 0; i < 40; i++) {
+                let word = words[i].word
+                let count = words[i].count
+                let random = Math.round((Math.random() * 4))
+                target.innerHTML += "<a class='word scale' style='font-size: " + (10 + (count * 2)) + "px; margin: " + random + "px;'> " + word + " </a>"
             }
-        }
-    })
-    // fetch('/crawling',
-    //     {
-    //         method: 'GET',
-    //         headers: {
-    //             'Content-Type': 'application/json; charset=UTF-8'
-    //         },
-    //     }).then((response => response.json())
-    //     .then((data) => {
-    //         console.log(data)
-    //         // for (const jsonElement of json) {
-    //         //     console.log(jsonElement.word + "\n")
-    //         //     console.log(jsonElement.count + "\n")
-    //         // }
-    //     })
-    // ).catch(error => {
-    //     console.error(error)
-    // })
+            loadingEnd(target)
+        })
+        .catch(() => {
+            target.innerText = "로딩 실패..."
+            loadingEnd(target)
+        })
+}
+
+// 로딩 중 이미지 표시
+function loadingImage(target) {
+    target.style.opacity = "0.5"
+    document.getElementById("target").innerHTML += "<img id='loadingImg' src='../image/spinner.gif'/>";
+}
+
+// 로딩 중 이미지 삭제
+function loadingEnd(target) {
+    target.style.opacity = "1.0"
+    document.getElementById("loadingImg").remove()
 }
 
 window.onload = function () {
