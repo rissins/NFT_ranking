75583b17d4340d0dfe69c9aeef21a27db5402011
ckdgus08 <ckdgus0808@naver.com>
2021-12-04 00:56:51 +0900

    style: 키워드 랜덤 색상 / 최소 크기 10, 최대크기 35

--- src/main/resources/static/js/keyword.js
+++ src/main/resources/static/js/keyword.js
@@ -4,13 +4,20 @@ function getKeyword() {
     loadingImage(target);
     axios.get('/crawling')
         .then((response) => {
-            let words = response.data.slice(0, 40).sort(() => Math.random() - 0.5);
+
+            let colors = ["#00933A", "#1E297E", "#492000", "#142d21", "#DE2663", "#B10024", "#006977", "#0020C0", "#FAAB00"]
+            let words = response.data.slice(0, 40)
+
+            let max = words[0].count
+            let min = words[39].count
+
+            words = words.sort(() => Math.random() - 0.5);
 
             for (let i = 0; i < 40; i++) {
                 let word = words[i].word
                 let count = words[i].count
                 let random = Math.round((Math.random() * 4))
-                target.innerHTML += "<a class='word scale' style='font-size: " + (10 + (count * 2)) + "px; margin: " + random + "px;'> " + word + " </a>"
+                target.innerHTML += "<a class='word scale' style='color: " + colors[Math.floor(Math.random() * 9)] + ";font-size: " + Math.round(10 + 25 * (count / (max + min))) + "px; margin: " + random + "px;'> " + word + " </a>"
             }
             loadingEnd(target)
         })
