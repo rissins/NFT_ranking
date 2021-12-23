13421ad84d4bf3e299efc4bad2ec508973514831
ckdgus08 <ckdgus0808@naver.com>
2021-12-24 06:02:35 +0900

    fix: 크롬 드라이버 동작 잘 안됨. 사용하는 부분 일단 제거

--- src/main/java/com/github/teamdon/teamdon/utils/htmlcrawling/ChosunCrawler.java
+++ src/main/java/com/github/teamdon/teamdon/utils/htmlcrawling/ChosunCrawler.java
@@ -1,37 +1,37 @@
-package com.github.teamdon.teamdon.utils.htmlcrawling;
-
-import com.github.teamdon.teamdon.utils.ChromeDriverBuilder;
-import lombok.RequiredArgsConstructor;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@Component
-public class ChosunCrawler implements Crawler {
-
-	@Override
-	public List<String> getWords() throws InterruptedException {
-		List<String> crawlingTexts = new ArrayList<>();
-
-		ChromeDriver driver = ChromeDriverBuilder.chromeDriver;
-
-		String URL = Site.CHOSUN.getUrl() + 1;
-		driver.get(URL);
-
-		Thread.sleep(1500);
-
-		List<WebElement> storyCards = driver.findElements(By.className("story-card-wrapper"));
-
-		for (WebElement storyCard : storyCards) {
-			WebElement title = storyCard.findElement(By.className("story-card__headline-container"));
-			WebElement content = storyCard.findElement(By.className("story-card__deck"));
-			crawlingTexts.add(title.getText());
-			crawlingTexts.add(content.getText());
-		}
-		return crawlingTexts;
-	}
-}
+//package com.github.teamdon.teamdon.utils.htmlcrawling;
+//
+//import com.github.teamdon.teamdon.utils.ChromeDriverBuilder;
+//import lombok.RequiredArgsConstructor;
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.WebElement;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@Component
+//public class ChosunCrawler implements Crawler {
+//
+//	@Override
+//	public List<String> getWords() throws InterruptedException {
+//		List<String> crawlingTexts = new ArrayList<>();
+//
+//		ChromeDriver driver = ChromeDriverBuilder.chromeDriver;
+//
+//		String URL = Site.CHOSUN.getUrl() + 1;
+//		driver.get(URL);
+//
+//		Thread.sleep(1500);
+//
+//		List<WebElement> storyCards = driver.findElements(By.className("story-card-wrapper"));
+//
+//		for (WebElement storyCard : storyCards) {
+//			WebElement title = storyCard.findElement(By.className("story-card__headline-container"));
+//			WebElement content = storyCard.findElement(By.className("story-card__deck"));
+//			crawlingTexts.add(title.getText());
+//			crawlingTexts.add(content.getText());
+//		}
+//		return crawlingTexts;
+//	}
+//}
